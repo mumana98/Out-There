08a8417b4ccb9d2a0f9fa38da1a44aca4684af2e
Natania Christopher <78063631+nataniac@users.noreply.github.com>
2021-04-05 22:18:37 -0500

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,5 @@
 function appendText() {
   event.preventDefault();
-  $("button").after("Thanks for reaching out! An Out There team member will get back to you shortly.");
+  var txt1 = $("<i></i>").text("love ");
+  $("button").after(txt1);
 }       
