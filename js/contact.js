e5426d6012462e8342eebd13843feaec32fe0daf
Natania Christopher <78063631+nataniac@users.noreply.github.com>
2021-04-05 22:24:22 -0500

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,4 @@
 function appendText() {
   event.preventDefault();
-  window.alert("Thanks for reaching out! An OutThere team member will get back to you shortly.);
+  window.alert("Thanks for reaching out! An OutThere team member will get back to you shortly.");
 }
