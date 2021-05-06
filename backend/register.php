073320b229527265cd16d6a6c11c1129c54a51b4
Matthew Umana <mumana98@gmail.com>
2021-05-05 22:37:39 -0500

    register modified - ready to test

--- backend/register.php
+++ backend/register.php
@@ -1,31 +1,58 @@
 <?php
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $username = $_POST['un'];
-        $password = $_POST['pw'];
-        setcookie('firstname', $_POST['fname'], time() + (10 * 365 * 24 * 60 * 60), '/');
-        setcookie('lastname', $_POST['lname'], time() + (10 * 365 * 24 * 60 * 60), '/');
-        setcookie('email', $_POST['email'], time() + (10 * 365 * 24 * 60 * 60), '/');
-        setcookie('city', $_POST['city'], time() + (10 * 365 * 24 * 60 * 60), '/');
-        setcookie('state', $_POST['state'], time() + (10 * 365 * 24 * 60 * 60), '/');
-        $taken = false;
-        $file = fopen("credentials.txt","r"); //read mode
 
-        while(!feof($file)){
-            $creds = explode(":", fgets($file));
-            if($username == $creds[0]){
-                $taken = true;
-                header("Location: ../pages/register.html"); 
-                break;
-            }
-        }
-        if($taken == false){
-            $fp = fopen('credentials.txt', 'a'); //append mode
-            fwrite($fp, "\n" . $username . ':' . $password); 
-            setcookie('loggedIn', true, time() + 120);
-            header("Location: ../pages/search.html"); 
-        }
-        fclose($file);
-        fclose($fp);
+    error_reporting(E_ALL);
+    ini_set("display_errors", "on");
+    
+    $server = "spring-2021.cs.utexas.edu";
+    $user   = "cs329e_bulko_umana";
+    $pwd    = "Being&cut\$Gun";
+    $dbName = "cs329e_bulko_umana";
 
+    // Connect to MySQL Server
+    $mysqli = new mysqli($server, $user, $pwd, $dbName);
+
+    if ($mysqli->connect_errno) {
+        die('Connect Error: ' . $mysqli->connect_errno . ": " .  $mysqli->connect_error);
     }
+    
+    //Select Database
+    $mysqli->select_db($dbName) or die($mysqli->error);
+
+    //------------------------------------------------------------------------------------------------------------------------
+
+    $username = $_GET["username"];
+    $password = $_GET["password"];
+    $firstname = $_GET["firstname"];
+    $lastname = $_GET["lastname"];
+    $email = $_GET["email"];
+    $city = $_GET["city"];
+    $state = $_GET["state"];
+
+    setcookie('firstname', $firstname, time() + (10 * 365 * 24 * 60 * 60), '/');
+    setcookie('lastname', $lastname, time() + (10 * 365 * 24 * 60 * 60), '/');
+    setcookie('email', $email, time() + (10 * 365 * 24 * 60 * 60), '/');
+    setcookie('city', $city, time() + (10 * 365 * 24 * 60 * 60), '/');
+    setcookie('state', $state, time() + (10 * 365 * 24 * 60 * 60), '/');
+
+    $username = $mysqli->real_escape_string($username);
+    $password = $mysqli->real_escape_string($password);
+    $firstname = $mysqli->real_escape_string($firstname);
+    $lastname = $mysqli->real_escape_string($lastname);
+    $email = $mysqli->real_escape_string($email);
+    $city = $mysqli->real_escape_string($city);
+    $state = $mysqli->real_escape_string($state);
+
+    //check for already registered users
+    $query = "SELECT * FROM USERS WHERE USERNAME = '$username' OR EMAIL = '$email'"; //username or email already exists
+    $result = $mysqli->query($query) or die($mysqli->error);
+    if(mysqli_num_rows($result) > 0){
+        echo "<p style='color:#f00'>Username or Email already registered</p>";
+    } 
+    else{
+        //insert user
+        $query = "INSERT INTO USERS (USERNAME, PASSWORD, FIRSTNAME, LASTNAME, EMAIL, STATE, CITY)
+                    VALUES ('$username', '$password', '$firstname', '$lastname', '$email', '$city', '$state');";
+        header("Location: ../pages/search.html");
+    }
+
 ?>
