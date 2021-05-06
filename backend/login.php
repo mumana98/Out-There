fc717d35f7d201c937bce57f2949b79098c63c93
Matthew Umana <mumana98@gmail.com>
2021-05-06 00:12:06 -0500

    login implemented and put in the backend - ready to test

--- backend/login.php
+++ backend/login.php
@@ -1,30 +1,45 @@
 <?php
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $username = $_POST['un'];
-        $password = $_POST['pw'];
-        $file = fopen("credentials.txt", "r");
-        while(!feof($file)){
+    error_reporting(E_ALL);
+    ini_set("display_errors", "on");
+    
+    $server = "spring-2021.cs.utexas.edu";
+    $user   = "cs329e_bulko_umana";
+    $pwd    = "Being&cut\$Gun";
+    $dbName = "cs329e_bulko_umana";
 
-            $creds = explode(":", fgets($file)); 
-            $a = str_replace(array("\n", "\r"), '', $creds[0]);
-            $b = str_replace(array("\n", "\r"), '', $creds[1]);
+    // Connect to MySQL Server
+    $mysqli = new mysqli($server, $user, $pwd, $dbName);
 
-            if(($username == $a) && ($password == $b)){
-                echo "<p>" . "Logging In" . "</p>";
+    if ($mysqli->connect_errno) {
+        die('Connect Error: ' . $mysqli->connect_errno . ": " .  $mysqli->connect_error);
+    }
+    
+    //Select Database
+    $mysqli->select_db($dbName) or die($mysqli->error);
 
-                ob_start();
-                setcookie('loggedIn', true, time() + 3600 * 3, '/');
-                ob_end_flush();
+    //------------------------------------------------------------------------------------------------------------------------
 
-                $_COOKIE['loggedIn'] = true;
-                header("Location: ../pages/search.html");
-                fclose($file);
-                break;
-            }
+    $username = $_GET["username"];
+    $password = $_GET["password"];
+
+    $username = $mysqli->real_escape_string($username);
+    $password = $mysqli->real_escape_string($password);
+
+    //check for already registered users
+    $query = "SELECT * FROM USERS WHERE USERNAME = '$username' AND PASSWORD = '$password'"; //find user with exact credentials
+    $result = $mysqli->query($query) or die($mysqli->error);
+    if(mysqli_num_rows($result) == 0){
+        echo "<p style='color:#f00'>Username or password was incorrect</p>";
+    } 
+    else{
+        while ($row = $result->fetch_row()) {
+            setcookie('firstname', $row[3], time() + (10 * 365 * 24 * 60 * 60), '/');
+            setcookie('lastname', $row[4], time() + (10 * 365 * 24 * 60 * 60), '/');
+            setcookie('email', $row[5], time() + (10 * 365 * 24 * 60 * 60), '/');
+            setcookie('state', $row[6], time() + (10 * 365 * 24 * 60 * 60), '/');
+            setcookie('city', $row[7], time() + (10 * 365 * 24 * 60 * 60), '/');
+            
+            echo "OK";
         }
-        echo "<p style='color:red'>" . "Incorrect Username or Password" . "</p>";
-        setcookie('login_fail', true, time() + 160, '/');
-        // header("Location: ../pages/login.html"); 
-        header("Location: ../pages/search.html"); //delete this
     }
 ?>
