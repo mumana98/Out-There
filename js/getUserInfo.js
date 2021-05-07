8e38b126f1be08d410a1050b4857212243f4fcc8
Matthew Umana <mumana98@gmail.com>
2021-05-07 13:36:03 -0500

    populate userInfo onto profile page

--- js/getUserInfo.js
+++ js/getUserInfo.js
@@ -13,6 +13,10 @@ function ObtainUserInfo(){
         if (ajaxRequest.readyState == 4) {
             userInfo = ajaxRequest.responseText;
             userInfo = userInfo.split(" ");
+            $("#name").html(userInfo[0] + " " + userInfo[1])
+            $("#email").html(userInfo[2])
+            $("#city").html(userInfo[3])
+            $("#state").html(userInfo[4])
             console.log(userInfo)
         }
     }
