613caea195ffe52a57974fffb2a223b70e9f9a45
Matthew Umana <mumana98@gmail.com>
2021-05-07 04:47:37 -0500

    profile page styling and data retrieval

---
+++ js/getUserInfo.js
@@ -0,0 +1,17 @@
+function ObtainUserInfo(){
+    var ajaxRequest;
+
+    ajaxRequest = new XMLHttpRequest();
+
+    var userInfo = ""
+
+    ajaxRequest.onreadystatechange = function () {
+        if (ajaxRequest.readyState == 4) {
+            userInfo = ajaxRequest.responseText;
+            console.log(userInfo)
+        }
+    }
+
+    ajaxRequest.open("GET", "../backend/getUserInfo.php", true);
+    ajaxRequest.send(null);
+}
