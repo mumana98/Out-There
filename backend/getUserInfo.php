876116c37a3cb91bcc6462fd06b8a767ce3bd3fa
Matthew Umana <mumana98@gmail.com>
2021-05-07 04:55:54 -0500

    spaces added for php echos

--- backend/getUserInfo.php
+++ backend/getUserInfo.php
@@ -1,7 +1,7 @@
 <?php
-    echo $_COOKIE["firstname"];
-    echo $_COOKIE["lastname"];
-    echo $_COOKIE["email"];
-    echo $_COOKIE["city"];
-    echo $_COOKIE["state"];
+    echo $_COOKIE["firstname"] . " ";
+    echo $_COOKIE["lastname"]. " ";
+    echo $_COOKIE["email"]. " ";
+    echo $_COOKIE["city"]. " ";
+    echo $_COOKIE["state"]. " ";
 ?>
