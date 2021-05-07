0179ec6c4ecd7269f53ef457d6e560dd8dfb99ac
Matthew Umana <mumana98@gmail.com>
2021-05-07 03:04:05 -0500

    search ajax moved back to html

--- js/loggedIn.js
+++ js/loggedIn.js
@@ -33,24 +33,21 @@ function ajaxFunction() {
 
 }
 
-function getData() {
-    var ajaxRequest;  // The variable that makes Ajax possible!
+// function getData() {
+//     var ajaxRequest;
 
-    ajaxRequest = new XMLHttpRequest();
+//     ajaxRequest = new XMLHttpRequest();
 
-    // Create a function that will receive data sent from the server and will update
-    // the div section in the same page.
+//     ajaxRequest.onreadystatechange = function () {
+//       if (ajaxRequest.readyState == 4) {
+//         var ajaxDisplay = document.getElementById('ajaxDiv');
+//         ajaxDisplay.innerHTML = ajaxRequest.responseText;
+//       }
+//     }
 
-    ajaxRequest.onreadystatechange = function () {
-      if (ajaxRequest.readyState == 4) {
-        var ajaxDisplay = document.getElementById('ajaxDiv');
-        ajaxDisplay.innerHTML = ajaxRequest.responseText;
-      }
-    }
+//     var searchTerm = document.getElementById('searchTerm').value;
+//     var queryString = "?searchTerm=" + searchTerm;
 
-    var searchTerm = document.getElementById('searchTerm').value;
-    var queryString = "?searchTerm=" + searchTerm;
-
-    ajaxRequest.open("POST", "../backend/retrieveData.php" + queryString, true);
-    ajaxRequest.send(null);
-}
+//     ajaxRequest.open("POST", "../backend/retrieveData.php" + queryString, true);
+//     ajaxRequest.send(null);
+// }
