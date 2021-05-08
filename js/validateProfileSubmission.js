d6662f9d6ddf6b295eb6f4f7025be34521923f57
Matthew Umana <mumana98@gmail.com>
2021-05-07 20:57:19 -0500

    form validation

--- js/validateProfileSubmission.js
+++ js/validateProfileSubmission.js
@@ -6,11 +6,23 @@ form_el.addEventListener("submit", function(evt) {
 })
 
 function validate(){
-    var username = document.getElementById('un').value
-    var pw = document.getElementById('pw').value
-    var rpw = document.getElementById('rpw').value
+    var username = document.getElementById('username').value
+    var pw = document.getElementById('password').value
+    var fn = document.getElementById('firstname').value
+    var ln = document.getElementById('lastname').value
+    var em = document.getElementById('email').value
+    var st = document.getElementById('state').value
+    var ct = document.getElementById('city').value
 
-    var firstLetter =  username.charAt(0);
+    if(username == ""){window.alert("Missing Field"); return}
+    if(pw == ""){window.alert("Missing Field"); return}
+    if(fn == ""){window.alert("Missing Field"); return}
+    if(ln == ""){window.alert("Missing Field"); return}
+    if(em == ""){window.alert("Missing Field"); return}
+    if(st == ""){window.alert("Missing Field"); return}
+    if(ct == ""){window.alert("Missing Field"); return}
+
+    var firstLetter = username.charAt(0);
 
     if(!username || !pw || !rpw){
         window.alert("Invalid username or password")
@@ -54,6 +66,7 @@ function validate(){
     }
     else{
         window.alert("User Validated!")
+        ajaxFunction()
         return 
     }
 }
@@ -82,4 +95,44 @@ function hasUpperCase(str) {
 
 function hasDigit(str) {
     return (/[0-9]/.test(str));
+}
+
+
+
+
+function ajaxFunction() {
+            
+    var ajaxRequest;  // The variable that makes Ajax possible!
+
+    ajaxRequest = new XMLHttpRequest();
+
+    // Create a function that will receive data sent from the server and will update
+    // the div section in the same page.
+
+    ajaxRequest.onreadystatechange = function () {
+        if (ajaxRequest.readyState == 4) {
+            if(ajaxRequest.responseText == "OK"){
+                window.location.replace("./search.html");    
+            }
+            else{
+                var ajaxDisplay = document.getElementById('ajaxDiv');
+                ajaxDisplay.innerHTML = ajaxRequest.responseText;
+            }
+        }
+    }
+
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    var firstname = document.getElementById('firstname').value;
+    var lastname = document.getElementById('lastname').value;
+    var email = document.getElementById('email').value;
+    var city = document.getElementById('city').value;
+    var state = document.getElementById('state').value;
+
+    var queryString = "?username=" + username;
+
+    queryString +=  "&password=" + password + "&firstname=" + firstname + "&lastname=" + lastname + "&email=" + email + "&city=" + city + "&state=" + state;
+
+    ajaxRequest.open("POST", "../backend/register.php" + queryString, true);
+    ajaxRequest.send(null);
 }
