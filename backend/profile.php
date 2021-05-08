7cf361ec938836750027e0c877a14e5df6634218
Matthew Umana <mumana98@gmail.com>
2021-05-07 23:23:03 -0500

    null check

--- backend/profile.php
+++ backend/profile.php
@@ -13,24 +13,27 @@
     // $original_array=unserialize($fixed);
 
     // echo $original_array;   
-
-    foreach($data as $op) {
-        echo "<div class='center-content-card'>
-                <h2>$op[0]</h2>
-                <table style='text-align: left;'>
-                <tr>
-                    <td><strong>Organization:</strong> $op[1]</td>
-                    <td class='spacer'></td>
-                    <td><strong>Location:</strong> $op[2]</td>
-                </tr>
-                <tr>
-                    <td><strong>In-Person:</strong> $op[3]</td>
-                    <td class='spacer'></td>
-                    <td><strong>Date:</strong> $op[4]</td>
-                </tr>
-                </table>
-                <div class='hr'></div>
-                <a href='' class='button'>Volunteer Here</a>
-            </div>";
+    if($data){
+        foreach($data as $op) {
+            echo "<div class='center-content-card'>
+                    <h2>$op[0]</h2>
+                    <table style='text-align: left;'>
+                    <tr>
+                        <td><strong>Organization:</strong> $op[1]</td>
+                        <td class='spacer'></td>
+                        <td><strong>Location:</strong> $op[2]</td>
+                    </tr>
+                    <tr>
+                        <td><strong>In-Person:</strong> $op[3]</td>
+                        <td class='spacer'></td>
+                        <td><strong>Date:</strong> $op[4]</td>
+                    </tr>
+                    </table>
+                    <div class='hr'></div>
+                    <a href='' class='button'>Volunteer Here</a>
+                </div>";
+        }
+    }else{
+        echo "None";
     }
 ?>
