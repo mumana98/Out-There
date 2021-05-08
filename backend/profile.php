efa0eb18bc9a1038a3e031ed0e55a8f1acf2ffd3
Matthew Umana <mumana98@gmail.com>
2021-05-07 23:46:21 -0500

    null check

--- backend/profile.php
+++ backend/profile.php
@@ -3,8 +3,10 @@
     ini_set("display_errors", "on");
     session_start();
     //$data = unserialize($_COOKIE['Opportunities']);
-    $data = unserialize($_COOKIE['Opportunities']);
-
+    $data = "";
+    if(isset($_COOKIE['Opportunities'])){
+        $data = unserialize($_COOKIE['Opportunities']);
+    }
     // $fixed = preg_replace_callback(
     //     '/s:([0-9]+):"(.*?)";/',
     //     function ($matches) { return "s:".strlen($matches[2]).':"'.$matches[2].'";';     },
