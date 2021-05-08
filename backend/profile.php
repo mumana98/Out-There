f8bf503ccfdbc53b8ff586d68fc313018976dc05
Matthew Umana <mumana98@gmail.com>
2021-05-07 19:59:56 -0500

    parse serialized data

--- backend/profile.php
+++ backend/profile.php
@@ -3,8 +3,18 @@
     ini_set("display_errors", "on");
     session_start();
     $data = unserialize($_COOKIE['Opportunities']);
-    echo $data;
-    foreach($data as $op) {
+    $serialized = 'a:10:{s:7:"contact";s:1:"1";s:19:"profile_affiliation";s:23:"University, Inc.";s:18:"profile_first_name";s:3:"Ben";s:22:"profile_street_address";s:19:"8718 Tot Ave. S.";s:12:"profile_city";s:6:"Mobile";s:13:"profile_state";s:2:"AL";s:15:"profile_country";s:3:"USA";s:15:"profile_zipcode";s:5:"36695";s:18:"profile_home_phone";s:10:"2599494420";s:17:"profile_last_name";s:6:"Powers";}';
+
+    $fixed = preg_replace_callback(
+        '/s:([0-9]+):"(.*?)";/',
+        function ($matches) { return "s:".strlen($matches[2]).':"'.$matches[2].'";';     },
+        $serialized
+    );
+    $original_array=unserialize($fixed);
+
+    echo $original_array;   
+
+    foreach($original_array as $op) {
         echo "<div class='center-content-card'>
                 <h2>$op[0]</h2>
                 <table style='text-align: left;'>
